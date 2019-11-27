1c20aa4c989c4bbd37cb9f5d6bf4defcf44fbf33
tomaswallentinus <info@wallentinus.se>
2019-11-27 11:41:21 +0100

    Google and common functions
    Moved common functions to external file
    Added Google groups and orgunit lookup

--- webhook_callback.php
+++ webhook_callback.php
@@ -2,106 +2,11 @@
 $myUser = '';
 $myPass = '';
 $myDB = '';
-$base64_basiclogin = 'base64-encoded basic-login for micromdm';
-$micromdm_path = '';
-$path_temp='path to temporary storage';
-$path_signcert='path to pem cert and key';
 
 $pdo = new PDO ('mysql:host=localhost;charset=utf8mb4;dbname=' . $myDB, $myUser, $myPass);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $log_entry=json_decode(file_get_contents('php://input'));
-
-$computer=array();
-
-function signMobileConfig (string $file_content) {
-	$file_full_pathname=$path_temp . "temp.mobileconfig";
-	file_put_contents($file_full_pathname,$file_content);
-    openssl_pkcs7_sign(
-        $file_full_pathname,
-        $file_full_pathname.'.sig',
-        "file://" . realpath($path_signcert . "mdmprofile_sign_cert.pem"),
-        "file://" . realpath($path_signcert . "mdmprofile_sign_key.pem"),
-        [], 0
-    );
-    $signed = file_get_contents($file_full_pathname.'.sig');
-    unlink($file_full_pathname.'.sig');
-    unlink($file_full_pathname);
-    $trimmed = preg_replace('/(.+\n)+\n/', '', $signed, 1);
-    return $trimmed;
-}
-function DeviceConfigured($udid){
-	$payload=array(
-		"request_type"		=> 'DeviceConfigured',
-		"udid"			=> $udid
-	);
-	$data_string = json_encode($payload, JSON_UNESCAPED_SLASHES);
-	mdmComm($data_string);
-}
-function AccountConfiguration($udid,$shortname,$fullname){
-	$payload=array(
-		"request_type"					=> 'AccountConfiguration',
-		"udid"						=> $udid,
-		"skip_primary_setup_account_creation"		=> false,
-		"set_primary_setup_account_as_regular_user"	=> false,
-		"lock_primary_account_info" 			=> true,
-		"primary_account_username" 			=> $shortname,
-		"primary_account_full_name" 			=> $fullname,
-		"auto_setup_admin_accounts"			=> [array(
-			"short_name" => $shortname . "_1",
-			"password_hash" => ""
-		)]
-	);
-	$data_string = json_encode($payload, JSON_UNESCAPED_SLASHES);
-	mdmComm($data_string);
-}
-function InstallApplication($manifest,$udid){
-	$payload=array(
-		"request_type"		=> 'InstallApplication',
-		"udid"			=> $udid,
-		"manifest_url"		=> $manifest,
-		"management_flags"	=> 1
-	);
-	$data_string = json_encode($payload, JSON_UNESCAPED_SLASHES);
-	mdmComm($data_string);
-}
-function InstallProfile($udid,$profile){
-	$payload=array(
-		"request_type"		=> 'InstallProfile',
-		"udid"			=> $udid,
-		"payload"		=> signMobileConfig ($profile),
-		"management_flags"	=> 1
-	);
-	$data_string = json_encode($payload, JSON_UNESCAPED_SLASHES);
-	mdmComm($data_string);
-}
-function mdmComm($data){
-//	print_r($data);
-	$ch = curl_init($micromdm_path . '/v1/commands');
-	curl_setopt($ch, CURLOPT_VERBOSE, true);
-//	curl_setopt($ch, CURLOPT_STDERR, $out); 
-	curl_setopt($ch, CURLOPT_HEADER, true);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);                                                       
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');   
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	curl_setopt($ch, CURLOPT_FAILONERROR,true);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-		'Accept: application/json;charset=utf-8',
-		'Content-Type: application/json;charset=utf-8',
-		'Authorization: ' . $base64_basiclogin
-	));
-	$result = curl_exec($ch);
-	if (curl_error($ch)) {
-		$error_msg = curl_error($ch);
-	}
-//print_r(curl_getinfo($ch,CURLINFO_HEADER_OUT));
-	curl_close($ch);
-	if (isset($error_msg)) {
-		echo 'Error: ';
-		print_r($error_msg);
-	}
-}
 
 switch ($log_entry->topic) {
 	case "mdm.Connect":
